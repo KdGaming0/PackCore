0e7fad1ea6742e9f40f32ee94ed38c21a9463e39
KdGaming0 <97355992+KdGaming0@users.noreply.github.com>
2025-10-10 16:31:56 +0200

    Add sodium and iris version management for Minecraft 1.21.5 and 1.21.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,16 @@ val lavenderMdVersion = when (mcVersion) {
     MinecraftVersions.VERSION_1_21_5 -> project.properties["lavender_md_version_1_21_5"]
     else -> project.properties["lavender_md_version_1_21_5"]
 }
+val sodiumVersion = when (mcVersion) {
+    MinecraftVersions.VERSION_1_21_8 -> project.properties["sodium_version_1_21_8"]
+    MinecraftVersions.VERSION_1_21_5 -> project.properties["sodium_version_1_21_5"]
+    else -> project.properties["sodium_version_1_21_5"]
+}
+val irisVersion = when (mcVersion) {
+    MinecraftVersions.VERSION_1_21_8 -> project.properties["iris_version_1_21_8"]
+    MinecraftVersions.VERSION_1_21_5 -> project.properties["iris_version_1_21_5"]
+    else -> project.properties["iris_version_1_21_5"]
+}
 
 dependencies {
     modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.dependencies.fabric.fabricApiVersion}")
@@ -51,7 +61,7 @@ dependencies {
     modImplementation(include("io.wispforest.lavender-md:core:$lavenderMdVersion")!!)
     modImplementation(include("io.wispforest.lavender-md:owo-ui:$lavenderMdVersion")!!)
 
-    modCompileOnly("maven.modrinth:sodium:mc1.21.6-0.6.13-fabric")
-    modCompileOnly("maven.modrinth:iris:1.9.1+1.21.7-fabric")
+    modCompileOnly("maven.modrinth:sodium:$sodiumVersion")
+    modCompileOnly("maven.modrinth:iris:$irisVersion")
     modRuntimeOnly("com.terraformersmc:modmenu:$modmenuVersion")
 }
